d034fcd80cfca5b53c61d2723d96fbd9a6644835
ThijsWaalders <henk418@gmail.com>
2018-06-28 07:42:43 +0200

    fix: path to images was incorrect

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -151,7 +151,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/Restaurant-Reviews-App-Stage-1/img/${restaurant.photograph}`);
   }
 
   /**
