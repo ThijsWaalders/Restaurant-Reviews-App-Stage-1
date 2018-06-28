18792a0f7a506ae076c9d61d774782798ddaa0da
ThijsWaalders <henk418@gmail.com>
2018-06-28 07:53:22 +0200

    fix: image url's

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -151,7 +151,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/Restaurant-Reviews-App-Stage-1/img/${restaurant.photograph}`);
+    return (`https://thijswaalders.github.io/Restaurant-Reviews-App-Stage-1/img/${restaurant.photograph}`);
   }
 
   /**
